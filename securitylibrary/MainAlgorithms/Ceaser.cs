7436561711c1d7d334d1706fdae9e1f4e191f441
shahd-mohamed428 <20201700410@cis.asu.edu.eg>
2024-03-14 13:09:07 +0200

    ceaser is done

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -8,19 +8,64 @@ namespace SecurityLibrary
 {
     public class Ceaser : ICryptographicTechnique<string, int>
     {
+
+        string resultofcipher;
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            Console.WriteLine(plainText);
+            string y = "abcdefghijklmnopqrstuvwxyz";
+            int i = 0;
+            while(i<plainText.Length)
+            {
+                int idx = y.IndexOf(plainText[i]);
+                // Console.WriteLine(idx);
+                resultofcipher += y[((idx + key) % 26)];
+                Console.WriteLine(resultofcipher);
+                i++;
+            }
+            return resultofcipher.ToUpper();
         }
-
+        string resultofplaintext;
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
-        }
+            // throw new NotImplementedException();
+            Console.WriteLine(cipherText);
+            string y = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+            int j = 0;
+            while ( j < cipherText.Length)
+            {
+                int idx = y.IndexOf(cipherText[j]);
+                int z = (idx - key);
+                if (z < 0)
+                    z += 26;
+                resultofplaintext += y[(z % 26)];
+                j++;
+            }
 
+            return resultofplaintext.ToLower();
+        }
+        int key;
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            //throw new NotImplementedException();
+            string y = "abcdefghijklmnopqrstuvwxyz";
+            int k = 0;
+            while ( k < plainText.Length)
+            {
+
+                int idx1 = y.IndexOf(cipherText[k]);
+                int idx2 = y.IndexOf(plainText[k]);
+                key = (idx1 - idx2) % 26;
+                if (key < 0)
+                    key += 26;
+
+                k++;
+            }
+            return key;
         }
     }
 }
--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -15,12 +15,80 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            // calculate the number of columns based on the length of the key
+            int numofCols = key.Count;
+            // calculate the number of rows based on the length of the ciphertext and the number of columns
+            int numofRows = (int)Math.Ceiling((double)cipherText.Length / numofCols);
+            // initialize the matrix to store the ciphertext
+            char[,] mrx = new char[numofRows, numofCols];
+            // initialize the index to keep track of the position in the ciphertext string
+            int calc = 0;
+            // fill the matrix column by column
+            for (int j = 0; j < numofCols; j++)
+            {
+                // get the index of the current column in the key
+                int colIndex = key.IndexOf(j + 1);
+                // fill the column from top to bottom
+                for (int i = 0; i < numofRows; i++)
+                {
+                    if (calc < cipherText.Length)
+                    {
+                        mrx[i, colIndex] = cipherText[calc];
+                        calc++;
+                    }
+                    else
+                    {
+                        mrx[i, colIndex] = ' ';
+                    }
+                }
+            }
+            // read the matrix row by row to get the plaintext
+            StringBuilder plaintextBuilder = new StringBuilder();
+            for (int i = 0; i < numofRows; i++)
+            {
+                for (int j = 0; j < numofCols; j++)
+                {
+                    plaintextBuilder.Append(mrx[i, j]);
+                }
+            }
+            // remove any trailing spaces
+            string plaintext = plaintextBuilder.ToString().Trim();
+            return plaintext;
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            plainText = plainText.Replace(" ", "").Replace(".", "");
+            int numofColumns = key.Count;
+            int numofRows = (int)Math.Ceiling((double)plainText.Length / numofColumns);
+            plainText = plainText.PadRight(numofRows * numofColumns, '\0');
+            char[,] mtx = new char[numofRows, numofColumns];
+            int calc = 0;
+
+            for (int row = 0; row < numofRows; row++)
+            {
+                for (int col = 0; col < key.Count && calc < plainText.Length; col++)
+                { 
+                    mtx[row, col] = plainText[calc];
+                    calc++;
+                  
+                }
+            }
+            string c = "";
+
+            for (int col = 0; col < numofColumns; col++)
+            {
+                for (int row = 0; row < numofRows; row++)
+                {
+                    c += mtx[row, col];
+                }
+            }
+            return c;
+
+
         }
     }
 }
