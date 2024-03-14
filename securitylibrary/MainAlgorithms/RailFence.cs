4d26185bb451ccb2f565a378010f71644a3447eb
abdalla-am <106037708+Abdalla-ayman-official@users.noreply.github.com>
2024-03-14 16:54:18 +0200

    repeatingKeyVigenere and Railfence added and working with succeeding tests

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,61 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            plainText = plainText.ToLower();
+            int PTlength = plainText.Length;
+            int[] key = new int[PTlength];
+            int keylength = key.Length;
+            for (int i = 0; i < PTlength; i++)
+            {
+                if (plainText[i] == cipherText[1])
+                {
+                    key[i] = i;
+                }
+            }
+            for (int j = 0; j < keylength; j++)
+            {
+                string strciphtxt = Encrypt(plainText, key[j]);
+                strciphtxt = strciphtxt.ToLower();
+                if (String.Equals(cipherText, strciphtxt))
+                {
+                    return key[j];
+                }
+            }
+            return -1;
         }
-
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            String plainText = "";
+            cipherText = cipherText.ToLower();
+            double CPlength = cipherText.Length;
+            int pltlength = (int)Math.Ceiling(CPlength / key);
+            char[] matrix = cipherText.ToCharArray();
+            for (int i = 0; i < pltlength; i++)
+            {
+                for (int j = i; j < CPlength; j += pltlength)
+                {
+                    plainText += matrix[j];
+                }
+            }
+            plainText = plainText.ToLower();
+            return plainText;
         }
-
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            String cipherText = "";
+            plainText = plainText.ToLower();
+            int PLtlength = plainText.Length;
+            char[] matrix = plainText.ToCharArray();
+
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = i; j < PLtlength; j += key)
+                {
+                    cipherText += matrix[j];
+                }
+            }
+            return cipherText;
         }
     }
 }
